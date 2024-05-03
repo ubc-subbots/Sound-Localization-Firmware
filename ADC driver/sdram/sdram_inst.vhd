e47b37faabe586aa4bc1aa6885034c2b64017af4
Robert Pan <rpan0827@gmail.com>
2024-05-02 19:10:12 -0700

    started experimenting with IP

---
+++ ADC driver/sdram/sdram_inst.vhd
@@ -0,0 +1,27 @@
+	component sdram is
+		port (
+			sdram_wire_addr  : out   std_logic_vector(13 downto 0);                    -- addr
+			sdram_wire_ba    : out   std_logic;                                        -- ba
+			sdram_wire_cas_n : out   std_logic;                                        -- cas_n
+			sdram_wire_cke   : out   std_logic;                                        -- cke
+			sdram_wire_cs_n  : out   std_logic;                                        -- cs_n
+			sdram_wire_dq    : inout std_logic_vector(15 downto 0) := (others => 'X'); -- dq
+			sdram_wire_dqm   : out   std_logic_vector(1 downto 0);                     -- dqm
+			sdram_wire_ras_n : out   std_logic;                                        -- ras_n
+			sdram_wire_we_n  : out   std_logic                                         -- we_n
+		);
+	end component sdram;
+
+	u0 : component sdram
+		port map (
+			sdram_wire_addr  => CONNECTED_TO_sdram_wire_addr,  -- sdram_wire.addr
+			sdram_wire_ba    => CONNECTED_TO_sdram_wire_ba,    --           .ba
+			sdram_wire_cas_n => CONNECTED_TO_sdram_wire_cas_n, --           .cas_n
+			sdram_wire_cke   => CONNECTED_TO_sdram_wire_cke,   --           .cke
+			sdram_wire_cs_n  => CONNECTED_TO_sdram_wire_cs_n,  --           .cs_n
+			sdram_wire_dq    => CONNECTED_TO_sdram_wire_dq,    --           .dq
+			sdram_wire_dqm   => CONNECTED_TO_sdram_wire_dqm,   --           .dqm
+			sdram_wire_ras_n => CONNECTED_TO_sdram_wire_ras_n, --           .ras_n
+			sdram_wire_we_n  => CONNECTED_TO_sdram_wire_we_n   --           .we_n
+		);
+
